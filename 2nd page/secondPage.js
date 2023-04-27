af532d1dde5606df895a80893f5dda850e7330cd
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-04-27 20:53:40 +0200

    test

--- 2nd page/secondPage.js
+++ 2nd page/secondPage.js
@@ -307,7 +307,7 @@ const options = {
   defaultRadioInput.type = "radio";
   defaultRadioInput.id = "All";
   defaultRadioInput.name = "difficulty";
-  defaultRadioInput.value = " ";
+  defaultRadioInput.value = '';
   // defaultRadioInput.checked = true; // Pre-select the "All" radio button
   radio.appendChild(defaultRadioInput);
   defaultRadioInput.addEventListener("change", function () {
